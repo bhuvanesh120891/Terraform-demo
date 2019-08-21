c484461471b2e28f2ceb9983f5a1b749525def4c
root <root@ip-172-31-29-151.us-west-2.compute.internal>
2019-08-21 08:48:35 +0000

    removed provisioner

--- resources.tf
+++ resources.tf
@@ -1,12 +1,4 @@
 resource "aws_instance" "demo" {
   ami           = "${var.ami}"
   instance_type = "${var.instance}"
-
-provisioner "remote-exec" {
-   inline = ["sudo apt-get -qq install python -y"]
-   }
-
-connection {
-    user = "${var.username}"
-    }
   }
