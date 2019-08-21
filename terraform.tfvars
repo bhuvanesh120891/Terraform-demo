c0e6a77313551390f9f9830bb68699c759c2db37
root <root@ip-172-31-29-151.us-west-2.compute.internal>
2019-08-21 09:11:00 +0000

    fixed issue

---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+ami = "ami-0cfee17793b08a293"
+instance = "t2.micro"
+region = "us-east-1"
--- variables.tf
+++ variables.tf
@@ -1,9 +1,3 @@
-variable "instance" {
-  default = "t2.micro"
-}
-variable "region" {
-  default = "us-east-1"
-}
-variable "ami" {
-  default = "ami-2757f631"
-}
+variable "instance" {}
+variable "region"{}
+variable "ami" {}
